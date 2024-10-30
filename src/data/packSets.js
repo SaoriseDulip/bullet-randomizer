193ee2668ec814e6d56668da9c558197fd719821
Sita Dulip <hacker.dulip@gmail.com>
2024-10-30 14:55:24 -0400

    feat: adding pack set information and data

---
+++ src/data/packSets.js
@@ -0,0 +1,37 @@
+export const heroines = [
+    {
+        name: 'Bullet Heart',
+        shortName: 'heart',
+        type: 'Core Set'
+    },
+    {
+        name: 'Bullet Star',
+        shortName: 'star',
+        type: 'Core Set'
+    },
+    {
+        name: 'Bullet Orange',
+        shortName: 'orange',
+        type: 'Expansion'
+    },
+    {
+        name: 'Bullet Paw',
+        shortName: 'paw',
+        type: 'Expansion'
+    },
+    {
+        name: 'Bullet Pallet',
+        shortName: 'pallet',
+        type: 'Expansion'
+    },
+    {
+        name: 'Anne-Claire Penny Arcade Promo',
+        shortName: 'penny',
+        type: 'Promo'
+    },
+    {
+        name: 'Beethoven Promo',
+        shortName: 'beethoven',
+        type: 'Promo'
+    },
+];
