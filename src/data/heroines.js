7b0f462760b7bcef858d5ac7996044e9fa9701c8
Sita Dulip <hacker.dulip@gmail.com>
2024-10-30 14:33:49 -0400

    feat: adding bullet heart heroines

---
+++ src/data/heroines.js
@@ -0,0 +1,58 @@
+export const heroines = [
+    {
+        name: 'Adelheid Beckenbauer',
+        bossName: 'Fugue in D-Minor',
+        set: 'heart',
+        difficulty: 'easy',
+        shortCode: 'adl'
+    },
+    {
+        name: 'Ekolu Kapakahi',
+        bossName: '3 - That Which Points',
+        set: 'heart',
+        difficulty: 'easy',
+        shortCode: 'eko'
+    },
+    {
+        name: 'Esfir Volkova',
+        bossName: 'The Fanged Sun',
+        set: 'heart',
+        difficulty: 'easy',
+        shortCode: 'esf'
+    },
+    {
+        name: 'Ling-Ling Xiao',
+        bossName: 'Ghost in the Machine',
+        set: 'heart',
+        difficulty: 'easy',
+        shortCode: 'lng'
+    },
+    {
+        name: 'Mariel Martin',
+        bossName: 'Mecha Mary',
+        set: 'heart',
+        difficulty: 'difficulty',
+        shortCode: 'mar'
+    },
+    {
+        name: 'Rie Akagi',
+        bossName: 'The Yakuza',
+        set: 'heart',
+        difficulty: 'difficult',
+        shortCode: 'rie'
+    },
+    {
+        name: 'Senka Kasun',
+        bossName: 'The Bauk',
+        set: 'heart',
+        difficulty: 'difficulty',
+        shortCode: 'sen'
+    },
+    {
+        name: 'Young-Ja Kim',
+        bossName: 'Hell On Wheels',
+        set: 'heart',
+        difficulty: 'difficult',
+        shortCode: 'kim'
+    },
+];
